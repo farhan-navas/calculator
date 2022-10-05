de2cda894c589465d720b093894826e8f3feda9d
Farhan <farhanmnavas@gmail.com>
2022-10-05 23:04:12 +0800

    Style calculator better

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
+const operate = function(operator, x, y) {
+    x = Number(x)
+    y = Number(y)
+    switch (operator) {
+        case 'add':
+            return add(a, b)    
+        case 'subtract':
+            return subtract(a, b)
+        case 'multiply':
+            return multiply(a, b)
+        case 'divide':
+            return divide(a, b)
+    }
+}
+
