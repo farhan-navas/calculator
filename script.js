9ee29aac769841dc8d3228cfea6b5342c63d085e
Farhan <farhanmnavas@gmail.com>
2022-10-06 15:34:16 +0800

    Complete basic javascript function

--- script.js
+++ script.js
@@ -3,18 +3,54 @@ const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
-const operate = function(operator, x, y) {
+// For this operate function, have to make sure operator input is a string!
+function operate( x, operator, y) {
     x = Number(x)
     y = Number(y)
-    switch (operator) {
+    operator = operator.toString()
+    switch(operator) {
         case 'add':
-            return add(a, b)    
+        case '+':
+            return add(x, y) 
+            break
         case 'subtract':
-            return subtract(a, b)
+        case '-':
+            return subtract(x, y)
+            break
         case 'multiply':
-            return multiply(a, b)
+        case '*':
+            return multiply(x, y)
+            break
         case 'divide':
-            return divide(a, b)
+        case '/':
+            return divide(x, y)
+            break
     }
+}
+
+let results = 0
+const screen = document.body.querySelector(".screen")
+const numbers = document.body.querySelectorAll("[data-type='number']")
+const operators = document.body.querySelectorAll("[data-type='operator']")
+const resetAll = document.body.querySelector(".reset-all")
+
+let firstNumber = ""
+let secondNumber = ""
+let operator = ""
+
+resetAll.addEventListener('click', function() {
+    screen.textContent = ''
+})
+
+for (let number of numbers) {
+    number.addEventListener('click', function() {
+        screen.textContent += number.textContent
+    })
+}
+
+for (let operator of operators) {
+    operator.addEventListener('click', function() {
+        screen.textContent += operator.textContent
+    })
 }
 
