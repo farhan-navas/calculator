de19d5288153120c11da66ce52bc43d7cb66da4e
Farhan <farhanmnavas@gmail.com>
2022-10-06 17:54:04 +0800

    Mostly fixed, unable to chain commands

--- script.js
+++ script.js
@@ -3,54 +3,102 @@ const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
-// For this operate function, have to make sure operator input is a string!
-function operate( x, operator, y) {
-    x = Number(x)
-    y = Number(y)
-    operator = operator.toString()
-    switch(operator) {
-        case 'add':
-        case '+':
-            return add(x, y) 
-            break
-        case 'subtract':
-        case '-':
-            return subtract(x, y)
-            break
-        case 'multiply':
-        case '*':
-            return multiply(x, y)
-            break
-        case 'divide':
-        case '/':
-            return divide(x, y)
-            break
-    }
-}
-
-let results = 0
-const screen = document.body.querySelector(".screen")
+let results = ""
+const prevDisplayNumber = document.querySelector(".prevNumber")
+const currDisplayNumber = document.querySelector(".currNumber")
 const numbers = document.body.querySelectorAll("[data-type='number']")
 const operators = document.body.querySelectorAll("[data-type='operator']")
+const equal = document.body.querySelector(".equal")
 const resetAll = document.body.querySelector(".reset-all")
 
-let firstNumber = ""
-let secondNumber = ""
+let prevNumber = ""
+let currNumber = ""
 let operator = ""
 
 resetAll.addEventListener('click', function() {
-    screen.textContent = ''
+    prevNumber = ""
+    currNumber = ""
+    prevDisplayNumber.textContent = prevNumber
+    currDisplayNumber.textContent = currNumber
 })
 
 for (let number of numbers) {
-    number.addEventListener('click', function() {
-        screen.textContent += number.textContent
+    number.addEventListener('click', function(e) {
+        handleNumber(e.target.textContent)
     })
+}
+
+function handleNumber(number) {
+    if (currNumber.length <= 10 || currNumber == "" ) {
+        currNumber += number
+        currDisplayNumber.textContent = currNumber
+    } else if (currNumber == ""){
+        currNumber = number
+    } else {
+        currNumber = currNumber
+    }
 }
 
 for (let operator of operators) {
-    operator.addEventListener('click', function() {
-        screen.textContent += operator.textContent
+    operator.addEventListener('click', function(e) {
+        handleOperator(e.target.textContent)
     })
 }
 
+function handleOperator(op) {
+    if (!results == "") {
+        operator = op.toString()
+        prevNumber = results
+        results = ""
+        prevDisplayNumber.textContent = prevNumber + " " + operator
+        currNumber = ""
+        currDisplayNumber.textContent = currNumber
+    } else {
+        operator = op.toString()
+        prevNumber = currNumber
+        prevDisplayNumber.textContent = currNumber + " " + operator
+        currNumber = ""
+        currDisplayNumber.textContent = currNumber
+    }
+}
+
+
+equal.addEventListener('click', operate)
+
+// For this operate function, have to make sure operator input is a string!
+function operate() {
+    prevNumber = Number(prevNumber)
+    currNumber = Number(currNumber)
+    operator = operator.toString()
+    console.log("working here")
+    if (!prevNumber == "" || !currNumber == "") {
+        console.log("workin")
+        switch(operator) {
+            case 'add':
+            case '+':
+                results = add(prevNumber, currNumber)
+                console.log(prevNumber)
+                console.log(currNumber)
+                console.log(results)
+                break
+            case 'subtract':
+            case '-':
+                results = subtract(prevNumber, currNumber) 
+                break
+            case 'multiply':
+            case '*':
+                results = multiply(prevNumber, currNumber)  
+                break
+            case 'divide':
+            case '/':
+                results = divide(prevNumber, currNumber) 
+                break
+        }
+    }
+    displayResults()
+}
+
+function displayResults() {
+    prevDisplayNumber.textContent += " " + currNumber
+    currDisplayNumber.textContent = results 
+}
